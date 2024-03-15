e74b3cc66ca3ff3474e43ed9648b6a50a5cbd815
EZ <ezasypkina03@list.ru>
2024-03-15 23:29:02 +0500

    Задание 3-его модуля

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,6 +1,7 @@
 export * from './enums/convenience.enum.js';
 export * from './enums/housing-type.enum.js';
 export * from './enums/user-type.enum.js';
+export * from './enums/component.enum.js';
 export * from './comment.type.js';
 export * from './rent-offer.type.js';
 export * from './user.type.js';
